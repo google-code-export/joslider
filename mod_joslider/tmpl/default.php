c3835958911ca40deac6d823ee53e9c42b92fd81
matthieu.26 <matthieu.26@2038560e-f012-147c-c305-3b8e84915e1b>
2010-12-05 10:51:48 +0000

    # Fix notice message for font and vertical variable

--- mod_joslider/tmpl/default.php
+++ mod_joslider/tmpl/default.php
@@ -49,8 +49,8 @@ if ($params->get("activeCorner", 1) == 1) $activeCorner = "true"; else $activeCo
 if ($params->get("hideSpines", 0) == 1) $hideSpines = "true"; else $hideSpines = "false";
 if ($params->get("autoPlay", 0) == 1) $autoPlay = "true"; else $autoPlay = "false";
 if ($params->get("cycle", 0) == 1) $cycle = "true"; else $cycle = "false";
-if ($params->get("cufonRefresh", 0) == 1) $font = "Cufon.replace('dl.slidedeck dt');";
-if ($params->get("vertical_slide", 1) == 1 && $params->get("slidedeck_pro", 0) == 1) $vertical = ".vertical()";
+if ($params->get("cufonRefresh", 0) == 1) $font = "Cufon.replace('dl.slidedeck dt');"; else $font = "";
+if ($params->get("vertical_slide", 1) == 1 && $params->get("slidedeck_pro", 0) == 1) $vertical = ".vertical()"; else $vertical = "";
 
 $doc->addScriptDeclaration("
 jQuery.noConflict();
